aae331c9f74ca6690ed198273db919f6c8e59b88
tobiasKaminsky <tobias@kaminsky.me>
2019-03-22 12:09:48 +0100

    reactivate tests
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/androidTest/java/com/owncloud/android/AbstractIT.java
+++ src/androidTest/java/com/owncloud/android/AbstractIT.java
@@ -1,3 +1,29 @@
+/* Nextcloud Android Library is available under MIT license
+ *
+ *   @author Tobias Kaminsky
+ *   Copyright (C) 2019 Tobias Kaminsky
+ *   Copyright (C) 2019 Nextcloud GmbH
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ *   BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ *   ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ *
+ */
 package com.owncloud.android;
 
 import android.content.Context;
@@ -10,6 +36,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import com.owncloud.android.lib.common.OwnCloudBasicCredentials;
 import com.owncloud.android.lib.common.OwnCloudClient;
 import com.owncloud.android.lib.common.OwnCloudClientFactory;
+import org.apache.commons.io.FileUtils;
 import com.owncloud.android.lib.resources.files.ReadFolderRemoteOperation;
 import com.owncloud.android.lib.resources.files.RemoveFileRemoteOperation;
 import com.owncloud.android.lib.resources.files.model.RemoteFile;
@@ -18,6 +45,9 @@ import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 /**
@@ -28,6 +58,12 @@ import java.util.ArrayList;
 public abstract class AbstractIT {
     protected static OwnCloudClient client;
     protected static Context context;
+
+    protected String baseFolderPath = "/test_for_build/";
+
+    public static final String ASSETS__TEXT_FILE_NAME = "textFile.txt";
+    
+    private static Context context;
 
     @BeforeClass
     public static void beforeAll() {
@@ -54,5 +90,13 @@ public abstract class AbstractIT {
                 new RemoveFileRemoteOperation(remoteFile.getRemotePath()).execute(client);
             }
         }
+    }
+
+    public static File getFile(String filename) throws IOException {
+        InputStream inputStream = context.getAssets().open(filename);
+        File temp = File.createTempFile("file", "file");
+        FileUtils.copyInputStreamToFile(inputStream, temp);
+
+        return temp;
     }
 }
